2f343abb239956652063bdfa220387eb48bb3657
TomohiroTatsumi <zm18140@josai.ac.jp>
2020-11-17 02:17:03 +0000

    1117

--- ex6-6.js
+++ ex6-6.js
@@ -2,4 +2,4 @@
   function sayhello66(){alert('Hello,'+ who66 +'!');}
   function taro66(){who66='太郎';}
   function hanako66(){who66='花子';}
-  function gakita(){alert('が来た');}
+  function gakita(){alert(who66 + 'が来た');}
