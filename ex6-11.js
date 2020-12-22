ab310347f5aaac15aa80bcd0a1569c5bf7b956f8
TomohiroTatsumi <zm18140@josai.ac.jp>
2020-12-22 02:12:48 +0000

    1222

--- ex6-11.js
+++ ex6-11.js
@@ -1,2 +1,5 @@
-  setTimeout('alert("3seconds have passed");',3000);
-  function imgchange1(){document.getElementById('logo').src='./image/bulbon.jpg';}
+  //setTimeout('alert("3seconds have passed");',3000);
+  //function imgchange1(){document.getElementById('logo').src='./image/bulbon.jpg';}
+
+  function ThreeSecChange() {setInterval("imgchange2()",3000);}
+  function imgchange2(){document.getElementById('logo').src='./image/bulbon.jpg';}
