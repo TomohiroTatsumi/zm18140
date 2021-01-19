a3e5828fbf2d500d745f3fc4fd7a157414418d5f
TomohiroTatsumi <zm18140@josai.ac.jp>
2021-01-19 02:28:05 +0000

    0119

--- ex6-11.js
+++ ex6-11.js
@@ -1,5 +1,2 @@
-  //setTimeout('alert("3seconds have passed");',3000);
-  //function imgchange1(){document.getElementById('logo').src='./image/bulbon.jpg';}
-
   function ThreeSecChange() {setInterval("imgchange2()",3000);}
   function imgchange2(){document.getElementById('logo').src='./image/bulbon.jpg';}
